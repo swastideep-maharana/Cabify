194e848b964cc4ad6dcab90e6ae99a32cc2cd964
swastideep maharana <swastideep67maharana@gmail.com>
2024-12-12 20:56:07 +0530

    # Vehiclepanel page done

---
+++ frontend/src/components/VehiclePanel.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+const VehiclePanel = (props) => {
+  return (
+    <div>
+      <h5
+        className="p-1 text-center absolute top-0 w-full "
+        onClick={() => {
+          props.setVehiclePanel(false);
+        }}
+      >
+        <i className="text-3xl cursor-pointer ri-arrow-down-wide-line"></i>
+      </h5>
+      <h3 className="text-2xl font-semibold mb-5">Choose a Vehicle</h3>
+      <div className="flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between">
+        <img
+          className="h-10"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdc1aAZzBJjW2GhT0TquUROYNGjc-8j6cSNeafEQAIpAOFJPXdwH_DrnzTOVen5HoFDRc&usqp=CAU"
+          alt="CabifyGo"
+        />
+        <div className="ml-2 w-1/2">
+          <h4 className="font-medium text-lg">
+            CabifyGo{" "}
+            <span>
+              <i className="ri-user-3-fill"></i>4
+            </span>
+          </h4>
+          <h5 className="font-normal text-xs text-gray-600">2 mins away</h5>
+          <p>Affordable, compact rides</p>
+        </div>
+        <h2 className="text-lg font-semibold">₹193.20</h2>
+      </div>
+      <div className="flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between">
+        <img
+          className="h-12"
+          src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png"
+          alt="Moto"
+        />
+        <div className="ml-2 w-1/2">
+          <h4 className="font-medium text-lg">
+            Moto{" "}
+            <span>
+              <i className="ri-user-3-fill"></i>1
+            </span>
+          </h4>
+          <h5 className="font-normal text-xs text-gray-600">3 mins away</h5>
+          <p>Affordable Motorcycle rides</p>
+        </div>
+        <h2 className="text-lg font-semibold">₹65.17</h2>
+      </div>
+      <div className="flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between">
+        <img
+          className="h-12"
+          src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png"
+          alt="Auto"
+        />
+        <div className="ml-2 w-1/2">
+          <h4 className="font-medium text-lg">
+            Auto{" "}
+            <span>
+              <i className="ri-user-3-fill"></i>3
+            </span>
+          </h4>
+          <h5 className="font-normal text-xs text-gray-600">2 mins away</h5>
+          <p>Affordable Auto rides</p>
+        </div>
+        <h2 className="text-lg font-semibold">₹118.1</h2>
+      </div>
+    </div>
+  );
+};
+
+export default VehiclePanel;
