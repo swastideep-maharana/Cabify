c7e19fc8b0c82942f69f2bd2f07023c86c732232
swastideep maharana <swastideep67maharana@gmail.com>
2024-12-13 21:57:39 +0530

    #captainDetails done

---
+++ frontend/src/components/CaptainDetails.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const CaptainDetails = () => {
+  return (
+    <div>
+      {" "}
+      <div className="flex items-center justify-between">
+        <div className="flex itesms-center justify-start gap-3">
+          <img
+            className="h-10 w-10 rounded-full object-cover"
+            src="https://img.freepik.com/free-photo/young-adult-man-wearing-hoodie-beanie_23-2149393636.jpg"
+            alt=""
+          />
+          <h4 className="text-lg font-medium">Harsh Patel</h4>
+        </div>
+        <div>
+          <h4 className="text-xl font-semibold">₹295</h4>
+          <p className="text-sm font-medium text-gray-600">Earned</p>
+        </div>
+      </div>
+      <div className="flex p-3 mt-6 bg-gray-100 rounded-xl justify-center gap-5 items-start">
+        <div className="text-center">
+          <i className="text-3xl mb-2 font-thin ri-time-line"></i>
+          <h5 className="text-lg font-medium">10.2</h5>
+          <p className="text-sm text-gry-600">Hours Online</p>
+        </div>
+        <div className="text-center">
+          <i className="text-3xl mb-2 font-thin ri-speed-up-line"></i>
+          <h5 className="text-lg font-medium">10.2</h5>
+          <p className="text-sm text-gry-600">Hours Online</p>
+        </div>
+        <div className="text-center">
+          <i className="text-3xl mb-2 font-thin ri-booklet-line"></i>
+          <h5 className="text-lg font-medium">10.2</h5>
+          <p className="text-sm text-gry-600">Hours Online</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainDetails;
--- frontend/src/pages/Captainhome.jsx
+++ frontend/src/pages/Captainhome.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import CaptainDetails from "../components/CaptainDetails";
 
 const Captainhome = () => {
   return (
-    <div>Captainhome</div>
-  )
-}
+    <div className="h-screen">
+      {/* Home Button */}
+      <div className="fixed p-6 top-0 flex items-center justify-between w-screen">
+        <header className="absolute left-5 top-5">
+          <h1 className="text-4xl font-bold text-gray-800 tracking-tight shadow-md">
+            Cabify
+          </h1>
+        </header>
+        <Link
+          to="/home"
+          className="fixed right-4 top-4 h-12 w-12 bg-white shadow-lg flex items-center justify-center rounded-full z-10 hover:shadow-xl transition-transform transform hover:scale-105"
+        >
+          <i class="ri-logout-box-r-line"></i>
+        </Link>
+      </div>
 
-export default Captainhome
+      <div className="h-3/5 p-6">
+        <img
+          className="h-full w-full object-cover rounded-xl shadow-lg"
+          src="https://simonpan.com/wp-content/themes/sp_portfolio/assets/uber-challenge.jpg"
+          alt="Background"
+        />
+      </div>
+
+      {/* Top Section with Image */}
+      <div className="h-2/5 p-6">
+          <CaptainDetails />
+      </div>
+    </div>
+  );
+};
+
+export default Captainhome;
