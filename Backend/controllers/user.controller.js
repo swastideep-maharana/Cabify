a647eb26d78d039a6430cd24439003c2ced9e29a
swastideep maharana <swastideep67maharana@gmail.com>
2024-12-16 20:18:41 +0530

    #did fsome fixx

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const userModel = require("../models/user.model");
 const userService = require("../services/user.service");
 const { validationResult } = require("express-validator");
-const BlacklistToken = require("../models/blackListToken.model");
+const BlacklistToken = require("../models/blacklistToken.model");
 
 module.exports.registerUser = async (req, res, next) => {
   const errors = validationResult(req);
@@ -66,7 +66,7 @@ module.exports.logoutUser = async (req, res, next) => {
   res.clearCookie("token");
   const token = req.cookies.token || req.headers.authorization.split(" ")[1];
 
-  await blackListTokenModel.create({ token });
+  await BlacklistToken.create({ token });
 
   res.status(200).json({ message: "Logged out" });
 };
