12a22b959933ea850ca82995dc9f9eb85339c52e
swastideep maharana <swastideep67maharana@gmail.com>
2024-12-16 20:24:17 +0530

    #auth fix

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,7 +1,8 @@
 const userModel = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const blackListTokenModel = require("../models/blackListToken.model");
+const BlacklistToken = require("../models/blacklistToken.model");
+
 const captainModel = require("../models/captain.model");
 
 module.exports.authUser = async (req, res, next) => {
@@ -11,7 +12,7 @@ module.exports.authUser = async (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
-  const isBlacklisted = await blackListTokenModel.findOne({ token: token });
+  const isBlacklisted = await BlacklistToken.findOne({ token: token });
 
   if (isBlacklisted) {
     return res.status(401).json({ message: "Unauthorized" });
